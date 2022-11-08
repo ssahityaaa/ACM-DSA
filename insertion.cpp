0732bb200458aad7084d6096e4bef88159a31c1e
Sahitya Karahe <97325402+ssahityaaa@users.noreply.github.com>
2022-11-08 23:11:23 +0530

    Create insertion.cpp

---
+++ insertion.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[100],i,j,k,l;
+    cout<<"Enter size of array"<<endl;
+    cin>>k;
+    cout<<"Enter elements in array "<<endl;
+    for(i=0;i<k;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Enter location to enter element"<<endl;
+    cin>>l;
+    cout<<"enter element to enter in array"<<endl;
+    cin>>j;
+   for(i=k-1;i>=l-1 ;i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[l-1]=j;
+    cout<<"corected array"<<endl;
+    for(i=0;i<=k;i++)
+    cout<<a[i]<<" ";
+    
+
+    return 0;
+}
